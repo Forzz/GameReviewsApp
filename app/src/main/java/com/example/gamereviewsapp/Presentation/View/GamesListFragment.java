c967ab7fa64bf1656e6b05f779d10b1a11bb118d
Max <38191334+Forzz@users.noreply.github.com>
2021-09-15 19:04:39 +0300

    Layout's setup (#2)
    * Add bottom bar, switching between fragments
    
    * Add some layouts, add packages for MVVM

---
+++ app/src/main/java/com/example/gamereviewsapp/Presentation/View/GamesListFragment.java
@@ -0,0 +1,44 @@
+package com.example.gamereviewsapp.Presentation.View;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.gamereviewsapp.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+
+public class GamesListFragment extends Fragment {
+
+    public GamesListFragment() {
+    }
+
+    public static GamesListFragment newInstance() {
+        return new GamesListFragment();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_games_list, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        BottomNavigationView bottomNavigationView = view.findViewById(R.id.bottomNavigationView);
+    }
+}
